wimleers/wpoanalytics-fb

commit 630c5937fe0faa2742c4a1f00847be48ce6bb1fa

Ensure that performance stats are also updated when signals don't arrive in the expected order.

code/UI/MainWindow.cpp
@@ -121,6 +121,13 @@ void MainWindow::updateAnalyzingStats(Time start, Time end, quint64 pageViews, q
                 .arg(QString::number(patternTreeSize))
                 .arg(QString::number(((12 + (patternTreeSize * (STATS_TILTED_TIME_WINDOW_BYTES + STATS_FPNODE_ESTIMATED_CHILDREN_AVG_BYTES))) / 1000.0 / 1000.0), 'f', 2))
     );
+
+    // Also update performance stats.
+    this->updateParsingDuration(this->totalParsingDuration);
+    if (this->totalTransactions)
+        this->updateAnalyzingDuration(this->totalAnalyzingDuration);
+    if (this->totalPatternsExaminedWhileMining)
+        this->updateMiningDuration(this->totalMiningDuration);
 }
 
 void MainWindow::minedRules(uint from, uint to, QList<Analytics::AssociationRule> associationRules, Analytics::SupportCount eventsInTimeRange) {

commit 4c999d86cf999a84642094f6b5d6c0a5f6e4930c

Correct the durations used to update the performance stats. Bug introduced in 630c5937fe.

code/UI/MainWindow.cpp
@@ -123,11 +123,11 @@ void MainWindow::updateAnalyzingStats(Time start, Time end, quint64 pageViews, q
     );
 
     // Also update performance stats.
-    this->updateParsingDuration(this->totalParsingDuration);
+    this->updateParsingDuration(0);
     if (this->totalTransactions)
-        this->updateAnalyzingDuration(this->totalAnalyzingDuration);
+        this->updateAnalyzingDuration(0);
     if (this->totalPatternsExaminedWhileMining)
-        this->updateMiningDuration(this->totalMiningDuration);
+        this->updateMiningDuration(0);
 }
 
 void MainWindow::minedRules(uint from, uint to, QList<Analytics::AssociationRule> associationRules, Analytics::SupportCount eventsInTimeRange) {